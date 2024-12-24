7089eec06255b500bf73b4e43bdfd25f46839c8e
devedroy <devedroy.dr@gmail.com>
2024-12-24 12:32:47 +0530

    Fetched data and showed relevant data, changed color of the text

--- app/src/main/java/com/myjar/jarassignment/ui/composables/AppNavigation.kt
+++ app/src/main/java/com/myjar/jarassignment/ui/composables/AppNavigation.kt
@@ -1,6 +1,7 @@
 package com.myjar.jarassignment.ui.composables
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,17 +10,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -32,15 +36,18 @@ fun AppNavigation(
     viewModel: JarViewModel,
 ) {
     val navController = rememberNavController()
-    val navigate = remember { mutableStateOf<String>("") }
+
 
     NavHost(modifier = modifier, navController = navController, startDestination = "item_list") {
+        composable("test_screen") {
+            Text("This is my test screen")
+        }
         composable("item_list") {
             ItemListScreen(
                 viewModel = viewModel,
-                onNavigateToDetail = { selectedItem -> navigate.value = selectedItem },
-                navigate = navigate,
-                navController = navController
+                onNavigateToDetail = { selectedItem ->
+                    navController.navigate("item_detail/$selectedItem")
+                },
             )
         }
         composable("item_detail/{itemId}") { backStackEntry ->
@@ -54,28 +61,66 @@ fun AppNavigation(
 fun ItemListScreen(
     viewModel: JarViewModel,
     onNavigateToDetail: (String) -> Unit,
-    navigate: MutableState<String>,
-    navController: NavHostController
 ) {
-    val items = viewModel.listStringData.collectAsState()
+    val items by viewModel.listStringData.collectAsStateWithLifecycle()
+    val searchResults by viewModel.searchResults.collectAsStateWithLifecycle()
 
-    if (navigate.value.isNotBlank()) {
-        val currRoute = navController.currentDestination?.route.orEmpty()
-        if (!currRoute.contains("item_detail")) {
-            navController.navigate("item_detail/${navigate.value}")
-        }
-    }
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        items(items.value) { item ->
-            ItemCard(
-                item = item,
-                onClick = { onNavigateToDetail(item.id) }
+    var searchQuery by rememberSaveable { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TextField(
+                value = searchQuery,
+                onValueChange = {
+                    if (it.isNotBlank()) {
+                        viewModel.onQueryChange(it)
+                    }
+                    searchQuery = it
+                },
+                modifier = Modifier.fillMaxWidth()
             )
-            Spacer(modifier = Modifier.height(8.dp))
+        }
+    ) { innerPadding ->
+        if (searchQuery.isNotBlank()) {
+            if (searchResults.isNotEmpty()) {
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    items(searchResults) { item ->
+                        ItemCard(
+                            item = item,
+                            onClick = { onNavigateToDetail(item.id) }
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                }
+            } else {
+                Text(text = "No items available for the searched query")
+            }
+        } else if (items.isNotEmpty()) {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(items) { item ->
+                    ItemCard(
+                        item = item,
+                        onClick = { onNavigateToDetail(item.id) }
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+            }
+        } else {
+            Text(text = "No items available")
         }
     }
 }
@@ -88,7 +133,15 @@ fun ItemCard(item: ComputerItem, onClick: () -> Unit) {
             .padding(8.dp)
             .clickable { onClick() }
     ) {
-        Text(text = item.name, fontWeight = FontWeight.Bold, color = Color.Transparent)
+        Text(text = item.name, fontWeight = FontWeight.Bold, color = Color.Black)
+        item.data?.let {
+            it.color?.let {
+                Text(text = it, fontWeight = FontWeight.Normal, color = Color.DarkGray)
+            }
+            it.price?.let {
+                Text(text = it.toString(), fontWeight = FontWeight.Normal, color = Color.DarkGray)
+            }
+        }
     }
 }
 
--- app/src/main/java/com/myjar/jarassignment/ui/vm/JarViewModel.kt
+++ app/src/main/java/com/myjar/jarassignment/ui/vm/JarViewModel.kt
@@ -1,13 +1,16 @@
 package com.myjar.jarassignment.ui.vm
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.myjar.jarassignment.createRetrofit
 import com.myjar.jarassignment.data.model.ComputerItem
 import com.myjar.jarassignment.data.repository.JarRepository
 import com.myjar.jarassignment.data.repository.JarRepositoryImpl
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class JarViewModel : ViewModel() {
@@ -16,11 +19,24 @@ class JarViewModel : ViewModel() {
     val listStringData: StateFlow<List<ComputerItem>>
         get() = _listStringData
 
+    private val _searchResults = MutableStateFlow<List<ComputerItem>>(emptyList())
+    val searchResults = _searchResults.asStateFlow()
+
+
     private val repository: JarRepository = JarRepositoryImpl(createRetrofit())
 
     fun fetchData() {
-        viewModelScope.launch {
-            repository.fetchResults()
+        Log.d("Tester", "calling api")
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.fetchResults().collect {
+                Log.d("Tester-collect", it.toString())
+                _listStringData.value = it
+                Log.d("Tester-list", _listStringData.value.toString())
+            }
         }
+    }
+
+    fun onQueryChange(string: String) {
+        _searchResults.value = _listStringData.value.filter { it.name.contains(string) }
     }
 }
